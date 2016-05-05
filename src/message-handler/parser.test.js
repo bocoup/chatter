470bb7de3266a8d28957e9cd4fbaeb7ecadc4a3f
Ben Alman <cowboy@rj3.net>
2016-05-05 12:24:19 -0400

    Add more parser tests.

--- src/message-handler/parser.test.js
+++ src/message-handler/parser.test.js
@@ -25,6 +25,42 @@ describe('ParsingMessageHandler', function() {
       return expect(parser.handleMessage()).to.be.fulfilled();
     });
 
+    it('should pass parsed args and additional arguments into child handlers', function() {
+      const parser = createParser({
+        handleMessage(args, a, b) {
+          return {args, a, b};
+        },
+        parseOptions: {
+          xxx: String,
+          yyy: Number,
+          zzz: Boolean,
+        },
+      });
+      return Promise.all([
+        expect(parser.handleMessage('foo bar', 1, 2)).to.become({
+          args: {
+            input: 'foo bar',
+            errors: [],
+            options: {},
+            remain: ['foo', 'bar'],
+          },
+          a: 1,
+          b: 2,
+        }),
+        expect(parser.handleMessage('foo bar x=1 y=2 z=3 baz', 1, 2)).to.become({
+          args: {
+            input: 'foo bar x=1 y=2 z=3 baz',
+            errors: [],
+            options: {xxx: '1', yyy: 2, zzz: true},
+            remain: ['foo', 'bar', 'baz'],
+          },
+          a: 1,
+          b: 2,
+        }),
+      ]);
+    });
+
   });
 
 });
+
