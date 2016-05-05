a88675b591d9915c6c08cc61adcb761f93ffc7c2
Ben Alman <cowboy@rj3.net>
2016-05-05 10:58:41 -0400

    Add promises / eslint support into chai.

--- tools/test-globals.js
+++ tools/test-globals.js
@@ -1,4 +1,9 @@
-import {assert, expect} from 'chai';
+import chai, {assert, expect} from 'chai';
+import dirtyChai from 'dirty-chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+chai.use(dirtyChai);
 
 global.assert = assert;
 global.expect = expect;
