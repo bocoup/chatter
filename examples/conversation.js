c61c3fa31e2b66e08e10edea886dc87e7f69fdec
Ben Alman <cowboy@rj3.net>
2016-05-06 14:38:58 -0400

    Conversation example.
    * Run with: npm run babel examples/conversation.js

---
+++ examples/conversation.js
@@ -0,0 +1,94 @@
+import createConversation from '../src/message-handler/conversation';
+import createMatcher from '../src/message-handler/matcher';
+import createParser from '../src/message-handler/parser';
+import createBot from '../src/bot';
+
+// foo top level command's subcommands
+const barCommand = createMatcher({
+  match: 'bar',
+  handleMessage: createParser({
+    handleMessage(args, {user, channel}) {
+      return {response: `bar received <${args.input}> from <${user.name}>`};
+    },
+  }),
+});
+
+const bazCommand = createMatcher({
+  match: 'baz',
+  handleMessage(message, {user, channel}) {
+    return {response: `baz received <${message}> from <${user.name}>`};
+  },
+});
+
+const askCommand = createMatcher({
+  match: 'ask',
+  handleMessage(message, {user, channel}) {
+    return {
+      response: `Why do you want me to ask you a question, ${user.name}?`,
+      dialog(message, {user, channel}) {
+        return {response: `I'm not sure \`${message}\` is a good reason, ${user.name}.`};
+      },
+    };
+  },
+});
+
+// foo top-level command
+const fooCommand = createMatcher({
+  match: 'foo',
+  handleMessage: [
+    barCommand,
+    bazCommand,
+    askCommand,
+    (message, {user, channel}) => ({response: `foo received <${message}> from <${user.name}>`}),
+  ],
+});
+
+// qux top-level command
+const quxCommand = createMatcher({
+  match: 'qux',
+  handleMessage: createParser({
+    handleMessage(args, {user, channel}) {
+      throw new Error(`qux error`);
+    },
+  }),
+});
+
+// Bot
+const myBot = createBot({
+  createConversation,
+  handleMessage: [
+    fooCommand,
+    quxCommand,
+  ],
+});
+
+
+function simulate(username, message) {
+  const user = {name: username};
+  console.log(`[${username}] ${message}`);
+  const logBot = message => { console.log(`= ${message}\n`); };
+  return myBot.getConversation(user.name).handleMessage(message, {user})
+    .then(data => {
+      if (data === false) {
+        return logBot(`Sorry, I don't understand \`${message}\`.`);
+      }
+      logBot(data.response);
+    })
+    .catch(error => {
+      logBot(`Error encountered: \`${error.message}\`.`);
+    });
+}
+
+import Promise from 'bluebird';
+Promise.mapSeries([
+  () => simulate('cowboy', 'foo bar should be parsed by the bar command handler'),
+  () => simulate('cowboy', 'foo baz should be parsed by the baz handler'),
+  () => simulate('cowboy', 'xyz should not be parsed by anything'),
+  () => simulate('cowboy', 'qux should throw an exception'),
+  () => simulate('cowboy', 'foo ask'),
+  () => simulate('joe', 'foo should be parsed by the foo fallback handler'),
+  () => simulate('joe', 'foo ask'),
+  () => simulate('cowboy', 'because i said so?'),
+  () => simulate('joe', 'i dunno'),
+  () => simulate('cowboy', 'foo should be parsed by the foo fallback handler'),
+], f => f());
