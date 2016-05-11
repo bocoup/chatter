380f08825969b00849bcb0b49a7bb3c13643aa56
Ben Alman <cowboy@rj3.net>
2016-05-11 11:31:07 -0400

    Further flesh out SlackBot.

--- src/slack/index.js
+++ src/slack/index.js
@@ -1,25 +1,85 @@
+import Promise from 'bluebird';
 import {Bot} from '../bot';
 import {isMessage, normalizeMessage} from '../util/response';
 import createSlackMessageHandler from './message-handler/slack';
 
-class SlackBot extends Bot {
+export class SlackBot extends Bot {
   constructor(options = {}) {
     super(options);
-    this.slack = options.slack;
+    const {slack, name = 'Test Bot'} = options;
+    if (!slack) {
+      throw new TypeError('Missing required "slack" option.');
+    }
+    this.slack = slack;
+    this.name = name;
     this.overrideProperties(options);
+    this.bindEventHandlers(['open', 'error', 'message']);
+  }
+
+  createSlackMessageHandler(...args) {
+    return createSlackMessageHandler(this.slack, ...args);
+  }
+
+  bindEventHandlers(events) {
+    events.forEach(name => {
+      const method = this[`on${name[0].toUpperCase()}${name.slice(1)}`];
+      if (method) {
+        this.slack.on(name, method.bind(this));
+      }
+    });
   }
 
   overrideProperties(options) {
     const overrides = [
+      'onOpen',
+      'onError',
+      'onMessage',
+      'login',
+      'ignoreMessage',
       'getConversationId',
       'postMessage',
       'postMessageOptions',
+      'handleResponse',
+      'handleResponseError',
     ];
     overrides.forEach(name => {
       if (options[name]) {
         this[name] = options[name];
       }
     });
+  }
+
+  onOpen() {
+    console.log('Bot connected.');
+  }
+
+  onError(...args) {
+    console.log('Bot error.', args);
+  }
+
+  onMessage(message) {
+    if (this.ignoreMessage(message)) {
+      return;
+    }
+    Promise.try(() => {
+      const id = this.getConversationId(message);
+      return this.getMessageHandler(id).handleMessage(message);
+    })
+    .then(response => {
+      this.handleResponse(message, response);
+    })
+    .catch(error => {
+      this.handleResponseError(message, error);
+    });
+  }
+
+  login() {
+    this.slack.login();
+    return this;
+  }
+
+  ignoreMessage(message) {
+    return message.subtype === 'bot_message';
   }
 
   getConversationId(message) {
@@ -38,15 +98,23 @@ class SlackBot extends Bot {
 
   postMessageOptions(text) {
     return {
-      username: 'Default Bot Name',
+      username: this.name,
       text,
       unfurl_links: false,
       unfurl_media: false,
     };
   }
 
-  createSlackMessageHandler(...args) {
-    return createSlackMessageHandler(this.slack, ...args);
+  handleResponse(message, response) {
+    if (response === false) {
+      return;
+    }
+    this.postMessage(message, response);
+  }
+
+  handleResponseError(message, error) {
+    console.error(error.stack);
+    this.postMessage(message, `An error occurred: \`${error.message}\``);
   }
 
 }
---
+++ src/slack/index.test.js
@@ -0,0 +1,35 @@
+import createSlackBot, {SlackBot} from './index';
+
+const nop = () => {};
+
+const slack = {
+  on: nop,
+};
+
+describe('SlackBot', function() {
+
+  describe('API', function() {
+
+    it('createSlackBot should return an instance of Bot', function() {
+      const bot = createSlackBot({slack, createMessageHandler: nop});
+      expect(bot).to.be.an.instanceof(SlackBot);
+    });
+
+    it('should behave like Bot', function() {
+      expect(() => createSlackBot({slack, createMessageHandler: nop})).to.not.throw();
+      expect(() => createSlackBot()).to.throw(/missing.*createMessageHandler/i);
+    });
+
+    it('should throw if no slack option was specified', function() {
+      expect(() => createSlackBot({createMessageHandler: nop})).to.throw(/missing.*slack/i);
+      expect(() => createSlackBot({createMessageHandler: nop, slack})).to.not.throw();
+    });
+
+  });
+
+  describe('TBD', function() {
+
+  });
+
+});
+
