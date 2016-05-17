1e8120726a518bb441915923166314cd8536171a
Ben Alman <cowboy@rj3.net>
2016-05-17 12:32:40 -0400

    More WIP command handler.

--- src/message-handler/command.js
+++ src/message-handler/command.js
@@ -1,26 +1,27 @@
-import {MatchingMessageHandler} from './matcher';
+import {DelegatingMessageHandler} from './delegate';
+import createMatcher from './matcher';
 
-export class CommandMessageHandler extends MatchingMessageHandler {
+export class CommandMessageHandler extends DelegatingMessageHandler {
 
   constructor(options = {}, children) {
-    const {name, usage, description} = options;
-    const matchOptions = Object.assign({}, options, {match: name});
-    super(matchOptions, children);
-
+    super(options, children);
     this.isCommand = true;
-    this.name = name;
-    this.usage = usage;
-    this.description = description;
-
-    this.children = Array.isArray(this.children) ? [...this.children] : [this.children];
+    this.children = this.origChildren = Array.isArray(this.children) ? [...this.children] : [this.children];
     if (this.hasSubCommands()) {
       this.children.push(this.getHelpHandler());
     }
     this.children.push(this.getFallbackHandler());
+    const {name, usage, description} = options;
+    this.name = name;
+    this.usage = usage;
+    this.description = description;
+    if (name) {
+      this.children = createMatcher({match: name}, this.children);
+    }
   }
 
   getSubCommands() {
-    return this.children.filter(c => c.isCommand);
+    return this.origChildren.filter(c => c.isCommand);
   }
 
   hasSubCommands() {
@@ -28,18 +29,19 @@ export class CommandMessageHandler extends MatchingMessageHandler {
   }
 
   getUsage() {
-    return [
-      this.usage && `Usage: \`${this.usage}\``,
-    ];
+    return this.usage && `Usage: \`${this.usage}\``;
   }
 
   getFallbackHandler() {
-    const {name} = this;
-    return message => [
-      message && `Unknown command \`${name} ${message}\`.`,
-      this.getUsage(),
-      this.hasSubCommands() && `Or try \`${name} help\` for more information.`,
-    ];
+    return message => {
+      const prefix = this.name ? `${this.name} ` : '';
+      const usage = this.getUsage();
+      return [
+        message && `Unknown command *${prefix}${message}*.`,
+        usage,
+        this.hasSubCommands() && `${usage ? 'Or try' : 'Try'} *${prefix}help* for more information.`,
+      ];
+    };
   }
 
   getHelpHandler() {
